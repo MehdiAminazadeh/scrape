5f104325b706f7e66d28228c4dc6bdd4b9b97c62
Mehdi Aminazadeh <mehdi.2000.aminazadeh@gmail.com>
2023-02-11 14:56:54 +0330

    update

--- website.py
+++ website.py
@@ -5,21 +5,31 @@ import os
 import json
 
 from bs4 import BeautifulSoup
-import pandas as pd
+
 from collections import defaultdict
 from contextlib import contextmanager
 from time import ctime, sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from requests.exceptions import ConnectionError
+from pathlib import Path
 
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+# import seaborn as sns
+# import pygal
 from selectors import selector
 
 logging.basicConfig(filename='log.log', level=logging.DEBUG)
+# sns.set()
 
 class Website:
     def __init__(self) -> None:
         self.mainUrl = 'https://tradingeconomics.com/'
+        self.selenium = Selenium()
+        self.saving = SaveConvention()
         
     def makeSoup(self, givenUrl):
         try:
@@ -28,7 +38,7 @@ class Website:
         
         except ConnectionError as ce: 
             return f'{ce} enable vpn'
-        
+    
     def Links_Overview(self):
         dict1 = defaultdict(dict)
         dict2 = {}
@@ -61,27 +71,25 @@ class Website:
             self.ribbon = ribbonLst
         
     def extractAllNews(self):
-        selen = Selenium()
-        save = SaveConvention()
         newsDict = defaultdict(dict)
-        selen.find_page(self.ribbon[1])
-        selen.scroll(5)
-        news = selen.find_elements(By.XPATH,selector.NEWS)
-        header = selen.find_elements(By.XPATH, selector.HEADER)
+        self.selenium.find_page(self.ribbon[1])
+        self.selenium.scroll(5)
+        news = self.selenium.find_elements(By.XPATH,selector.NEWS)
+        header = self.selenium.find_elements(By.XPATH, selector.HEADER)
         for new, head in zip(news, header):
             if len(new.text) > len(head.text):
                 newsDict[head.text].update({
                     'News': new.text.replace('\n', ',')
                 })
-        if 'News.json' in os.listdir(save.path):
+        if 'News.json' in os.listdir(self.saving.path1):
             print('Already available')
+            
         else:
             file = open('News.json', mode='w')
             json.dump(newsDict, file)
             file.close()
         
-    def topNews(self, 
-                text=False):
+    def topNews(self, text=False):
         topNews = []
         page = self.makeSoup(self.mainUrl)
         news = page.find('div', class_='col-md-8')
@@ -94,7 +102,6 @@ class Website:
             else:
                 topNews.append(string)
         topNews.insert(0, ctime())
-        save = SaveConvention()
         if text:
             try:
                 with open('data.txt', 'r') as text:
@@ -102,31 +109,33 @@ class Website:
                     if lineOne[1] == topNews[1]:
                         print('Top News are Up to dated!')
                     else:
-                        save.saveText(topNews,changeInData=True)
+                        self.saving.saveText(topNews,changeInData=True)
             except FileNotFoundError:
-                save.saveText(topNews)
+                print('file not found, saving initial text file')
+                self.saving.saveText(topNews)
         else:
-            save.saveCsv(topNews)
+            self.saving.saveCsv(topNews)
+            
+    def moreIndicators(self):
+        pass
         
-    def indicators(self):
-        selen = Selenium()
-        selen.find_page(self.ribbon[-2])
-        indicatorHref = selen.find_elements(
+    def mainIndicators(self):
+        self.selenium.find_page(self.ribbon[-2])
+        indicatorHref = self.selenium.find_elements(
             By.XPATH, selector.INDICATOR_LINKS
             )
         links = [link.get_attribute('href') for link in indicatorHref] 
         foundElements = defaultdict(dict)
-        save = SaveConvention()
         try:
             for link in links:
-                selen.find_page(link)
-                world = selen.find_elements(
+                self.selenium.find_page(link)
+                world = self.selenium.find_elements(
                     By.XPATH, selector.LI_RIBBON
                     )[0]
                 world.click()
-                ribbon = selen.find_elements(By.XPATH, selector.TH_RIBBON)
+                ribbon = self.selenium.find_elements(By.XPATH, selector.TH_RIBBON)
                 values = [value.text for value
-                        in selen.find_elements(
+                        in self.selenium.find_elements(
                             By.XPATH, selector.TR_VALUE
                         )]
                 
@@ -144,30 +153,36 @@ class Website:
                         ribbon[4].text:lst[4],
                     }
                     )  
-                title = selen.find_element(
+                title = self.selenium.find_element(
                     By.XPATH, selector.TITLE
                     ).text.replace(' | World', '')
-                save.saveCsv(foundElements, title, transpose=True)
+                self.saving.saveCsv(foundElements, title, transpose=True)
         except IndexError:
             pass
-            to_click = selen.find_elements(By.XPATH, selector.LI_RIBBON)
+            to_click = self.selenium.find_elements(By.XPATH, selector.LI_RIBBON)
             to_click[0].click()
-            ribbon = selen.find_elements(By.XPATH, selector.TR_VALUE)
+            ribbon = self.selenium.find_elements(By.XPATH, selector.TR_VALUE)
             for rib in ribbon:
                 print(rib.text)
-              
+    
     def markets(self):
-        soup = self.makeSoup(self.ribbon[4])
-        pass
+        linksToScrape = []
+        self.selenium.find_page(self.ribbon[1])
+
+        links = self.selenium.find_elements(By.XPATH, selector.MARKETS)
+        for link in links:
+            fullLink = self.ribbon[1] + link.get_attribute('href')
+            linksToScrape.append(fullLink)
         
     def execute(self):
         self.Links_Overview()
         self.topNews(text=True)
         self.extractAllNews()
-        self.indicators()
-        # self.markets()
+        self.mainIndicators()
+        self.markets()
         
 class Selenium(webdriver.Chrome):
+    """Initiating chrome driver"""
     def __init__(self, 
                  driver= selector.DRIVER
                  ) -> None:
@@ -175,11 +190,13 @@ class Selenium(webdriver.Chrome):
         self.driver = driver
         self.implicitly_wait(20)
         self.maximize_window()
+        self.action = ActionChains(driver)
         
     def find_page(self, givenUrl):
         self.get(givenUrl)
+        print(self.get_network_conditions)
         
-    def scroll(self, limit:int, pause:int=2):
+    def scroll(self, limit:int, pause:int=2) -> None:
         scroll_count = 0
         prev_height = self.execute_script(selector.PAGE_HEIGHT)
         while True and scroll_count < limit:
@@ -190,23 +207,29 @@ class Selenium(webdriver.Chrome):
                 break
             prev_height = new_height
             scroll_count += 1
+            
+    def actionChain(self):
+        pass
         
-class SaveConvention:
-    def __init__(self):
-        try:
-            self.path = os.path.join(os.path.dirname(__file__), 'result')
-        except FileExistsError:
-            pass
-        
+class SaveConvention(object):
+    def __init__(self) -> None:
+        #hard coded paths 
+        self.path1 = os.path.join(os.path.dirname(__file__), 'result')
+        self.path2 = os.path.join(os.path.dirname(__file__), 'pltFigs') 
+    
+    def __repr__(self) -> str:
+        print('Paths available: {0}\n{1}'.format(self.path1, self.path2))
+    
     @contextmanager
     def changeDir(self, path):
+        origin = Path().absolute()
         try:
-            prev_cwd = os.getcwd()
             os.chdir(path)
+            yield
         finally:
-            yield os.chdir(prev_cwd)
+            os.chdir(origin)
             
-    def saveCsv(self, data, fileName, transpose=False):
+    def saveCsv(self, data, fileName:str, transpose:bool=False):
         with self.changeDir(self.path):
             if transpose: 
                 dataFrame = pd.DataFrame(data).T
@@ -215,38 +238,77 @@ class SaveConvention:
                 dataFrame = pd.DataFrame(data)
                 dataFrame.to_csv(f'{fileName}.csv')
             
-    def saveText(self, data, changeInData=False):
+    def saveText(self, data, changeInData:bool=False):
         with self.changeDir(self.path):
-            with open('data.txt', 'w') as text:
-                string = '\n'.join(data)
-                text.write(string)
             if changeInData:
                 with open('data.txt', 'r') as textRead:
                     with open('data.txt', 'w') as textWrite:
                         prevData = textRead.read()
                         newData = prevData + '\n'.join(data)
                         textWrite.write(newData)
+            else: 
+                with open('data.txt', 'w') as text:
+                    string = '\n'.join(data)
+                    text.write(string)
+# inst1 = Website()
+# inst1.execute()
 
-inst1 = Website()
-inst1.execute()
-
-# import tensorflow as tf
-# from tensorflow import keras
-# from keras.preprocessing.text import Tokenizer
-# from keras.utils import pad_sequences
-
-# class AnalyzeNews:
-#     def __init__(self, file) -> None:
-#         self.file = open(file, 'r').readlines()[1:]
+class Analyze(SaveConvention):
     
-#     def tokenize(self):
-#         tokenizer = Tokenizer(num_words=100, oov_token="<OOV>")
-#         tokenizer.fit_on_texts(self.file)  
-#         word_index = tokenizer.word_index
-#         sequence = tokenizer.texts_to_sequences(self.file)
-#         padded = pad_sequences(sequence, padding='post')
-#         print(padded[0])
-#         print(padded.shape)
+    """This class stores values taken from scraped 
+    files and does various analysis on them"""
+    def __init__(self,csvFile:list, jsonFile:list, textFile:list) -> None:
+        super().__init__()
+        self.csvFile = csvFile
+        self.json = jsonFile
+        self.text = textFile
         
-# a = AnalyzeNews('data.txt')
-# a.tokenize()
+    def nlp_nlu_news(self, givenFile=''):
+        if givenFile and givenFile.endswith('.txt'):
+            pass
+        else:
+            # json 
+            print('file extension is incorrect')
+    def nlp_nlu_rankings(self):
+        pass
+    
+    def scatter_Plot(self):
+        pass
+    
+    def bar_Plot(self, barh=False):
+        if barh:
+            pass
+        pass
+    
+    def heatmap(self):
+        pass
+    
+    def elbow_method(self, data, knum=10):
+        pass
+        
+    def cluster(self, cluster):
+        combined = self.combine_files('', '')
+        pass
+           
+    def Highest_lowest_ranks(self, rateFile):
+        pass
+    
+    def combine_files(self, first, second):
+        with self.changeDir(path=self.path1):
+            pass
+    
+    def save_result(self, *args, typeSave):
+        typesAllowed = ['.jpg', '.png', '.jpeg']
+        with self.changeDir(self.path2):
+            for element in args:
+                if typeSave in typesAllowed:
+                    plt.savefig(f'{element}.{typeSave}')
+                    print(f'{element}.{typeSave} stored in {self.path2}')
+                else:
+                    return
+                
+    def rates_on_map(self, rate):
+        pass
+    
+a = Analyze(['Inflation Rate.csv'])
+a.scatter()
